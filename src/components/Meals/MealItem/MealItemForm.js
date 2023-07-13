6bc47a74697dd28a57dad2fc604142bdb6c7a340
devanshuc <66375026+devanshuc@users.noreply.github.com>
2023-07-13 12:59:49 +0530

    Getting access to input through refs

--- src/components/Meals/MealItem/MealItemForm.js
+++ src/components/Meals/MealItem/MealItemForm.js
@@ -1,9 +1,17 @@
+import { useRef } from "react";
 import Input from "../../UI/Input";
 import styles from "./MealItemForm.module.css";
 const MealItemForm = (props) => {
+	const amountInputRef = useRef();
+
+	const submitHandler = (event) => {
+		event.preventDefault();
+	};
+
 	return (
-		<form className={styles.form}>
+		<form className={styles.form} onSubmit={submitHandler}>
 			<Input
+				ref={amountInputRef}
 				label="Amount"
 				input={{
 					id: "amount_" + props.id, // this changed!
