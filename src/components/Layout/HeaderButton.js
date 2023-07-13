7e5524335f0d550d9274e7f2a4b6c5e000692743
devanshuc <66375026+devanshuc@users.noreply.github.com>
2023-07-13 11:08:14 +0530

    Added Cart Context logic to main file

--- src/components/Layout/HeaderButton.js
+++ src/components/Layout/HeaderButton.js
@@ -1,13 +1,22 @@
 import styles from "./HeaderButton.module.css";
 import CartIcon from "../Cart/CartIcon";
+import CartContext from "../../store/cart-context";
+import { useContext } from "react";
+
 const HeaderButton = (props) => {
+	const cartCntxt = useContext(CartContext);
+
+	const numberOfCartItems = cartCntxt.items.reduce((currentNumber, item) => {
+		return currentNumber + item.amount;
+	}, 0);
+
 	return (
 		<button className={styles.button} onClick={props.onTouch}>
 			<span className={styles.icon}>
 				<CartIcon />
 			</span>
 			<span>Your Cart</span>
-			<span className={styles.badge}>3</span>
+			<span className={styles.badge}>{numberOfCartItems}</span>
 		</button>
 	);
 };
